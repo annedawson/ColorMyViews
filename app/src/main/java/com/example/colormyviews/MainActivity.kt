2a4cf3e1b8c60345afb9a72f4c657b8a93a9913f
Anne Dawson <35181687+annedawson@users.noreply.github.com>
2021-02-21 10:28:15 -0800

    At the end of UD9012-L2C27 - implement setListeners() and makeColored()

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,10 +1,27 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.colormyviews
 
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.colormyviews.databinding.ActivityMainBinding
-import android.view.View as View
+
 
 // added 19 Feb 2021
 // see https://youtu.be/omml4lK_b-A    from 7.08 min  VIEW BINDING
@@ -22,24 +39,48 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)  // the root layout of activity_main.xml
         // now all the views on the layout are accessible through the binding object
-        binding.boxOneText.setBackgroundColor(Color.DKGRAY)
+        binding.boxOneText.setBackgroundColor(Color.LTGRAY)
         binding.boxOneText.text = "Hello!!!"
         // test on Sat 20 Feb 2021
 
-        //setListeners()
+        setListeners()
     }
 
 
     /**
      * Attaches listeners to all the views.
      */
-    //private fun setListeners() {
-    //val clickableViews: List<View> = listOf(box_one_text)
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(binding.boxOneText, binding.boxTwoText, binding.boxThreeText, binding.boxFourText, binding.boxFiveText)
 
-    /*
-    for (item in clickableViews) {
-        item.setOnClickListener { makeColored(it) }
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
     }
-             */
-    //}
+
+
+    /**
+     * Sets the background color of a view depending on it's resource id.
+     * This is a way of using one click handler to do similar operations on a group of views.
+     *
+     * Demonstrates three different ways of setting colors.
+     *     * Using a color constant from the Color class.
+     *     * Using an Android color resource
+     *     * Using a custom color defined in color.xml
+     */
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.BLUE)
+        }
+    }
 }
